f96f9f60757a90faa37b57c4507aaa721cf69688
PoopFace <danilsteam-88@hotmail.com>
2023-11-08 01:20:31 +0300

    i

---
+++ README.md
@@ -0,0 +1,36 @@
+# Frontend Mentor - Time tracking dashboard solution
+
+This is a solution to the [Time tracking dashboard challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/time-tracking-dashboard-UIQ7167Jw). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### Screenshot
+
+![](./screenshot.png)
+
+### Links
+
+- Live Site URL: [https://spectacular-beignet-efd2d2.netlify.app/](https://spectacular-beignet-efd2d2.netlify.app/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Preprocessor SCSS
+- JavaScript
+- React
+
+## Author
+
+- Frontend Mentor - [@StinkyCodeMACHINE](https://www.frontendmentor.io/profile/StinkyCodeMACHINE)
---
+++ src/App.jsx
@@ -0,0 +1,103 @@
+import { useState } from "react";
+import json from "./json/data.json";
+
+function App() {
+  const [options, setOptions] = useState([
+    {
+      name: "Daily",
+      selected: false,
+    },
+    {
+      name: "Weekly",
+      selected: true,
+    },
+    {
+      name: "Monthly",
+      selected: false,
+    },
+  ]);
+
+  return (
+    <>
+      <main>
+        <h1 className="sr-only">Time tracking dashboard</h1>
+        <div className="dashboard">
+          <div className="dashboard__options-card">
+            <div className="dashboard__options-card__info">
+              <img src="/assets/image-jeremy.png" alt="your pic" />
+              <div>
+                <h2>Report for</h2>
+                <div className="dashboard__options-card__info__name">
+                  Jeremy Robson
+                </div>
+              </div>
+            </div>
+            <div className="dashboard__options-card__options">
+              {options.map((option, index) => (
+                <div
+                  key={option.name}
+                  className={option.selected ? "selected" : ""}
+                  onClick={() => {
+                    if (
+                      options.find(
+                        (selectedOption) =>
+                          selectedOption.selected === true &&
+                          selectedOption.name !== option.name
+                      )
+                    ) {
+                      const newOptions = [...options];
+                      newOptions.find(
+                        (selectedOption) => selectedOption.selected
+                      ).selected = false;
+                      newOptions[index].selected = true;
+                      setOptions(newOptions);
+                    }
+                  }}
+                >
+                  {option.name}
+                </div>
+              ))}
+            </div>
+          </div>
+          {json.map((dataPiece) => (
+            <div
+              key={dataPiece.title}
+              className={`dashboard__info-card ${dataPiece.title
+                .toLocaleLowerCase()
+                .replace(" ", "-")}`}
+            >
+              <div className="dashboard__info-card__info">
+                <div className="dashboard__info-card__info__top-stuff">
+                  <h2>{dataPiece.title}</h2>
+                  <img src="/assets/icon-ellipsis.svg" alt="ellipsis" />
+                </div>
+                <div className="dashboard__info-card__info__bottom-stuff">
+                  <div className="dashboard__info-card__info__cur-stat">
+                    {`${
+                      dataPiece.timeframes[
+                        options
+                          .find((option) => option.selected === true)
+                          .name.toLowerCase()
+                      ].current
+                    }hrs`}
+                  </div>
+                  <div className="dashboard__info-card__info__prev-stat">
+                    {`Last Week - ${
+                      dataPiece.timeframes[
+                        options
+                          .find((option) => option.selected === true)
+                          .name.toLowerCase()
+                      ].previous
+                    }hrs`}
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
